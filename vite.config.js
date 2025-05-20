ce91b288942082fe07b885445c221ab0d0dfc60d
ABDUL REHMAN <abdul.reh311@gmail.com>
2025-05-21 04:04:31 +0500

    fff

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
     host: '0.0.0.0',  // or use your local IP (e.g., '192.168.x.x')
     port: 3001,       // Or whatever port your Vite server uses
   },
-  base: '/University/',  
+  base: '/UniProject/',  
 
 })
